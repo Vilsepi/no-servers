8f301a549333be3d964b4f0f2d0724f21ea74b09
Ville Seppanen <ville.seppanen@gofore.com>
2017-01-29 21:05:45 +0200

    Do not upload json files to frontend assets. Sync with delete.

--- frontend/upload.sh
+++ frontend/upload.sh
@@ -6,15 +6,15 @@ PROFILE="heap"
 BUCKET="serverless.heap.fi"
 
 PROFILE_ARGS="--profile $PROFILE --region $REGION"
-S3_SYNC_ARGS="s3://$BUCKET/ --exclude '*.map*' --exclude '*.gz' --exclude '*.sh'"
+S3_SYNC_ARGS="s3://$BUCKET/ --exclude '*.map*' --exclude '*.gz' --exclude '*.sh' --exclude '*.json'"
 
 if [ $# -lt 1 ]
 then
   echo "Simulating S3 sync, no changes are made. To deploy, run with --doit"
-  eval aws s3 sync . $S3_SYNC_ARGS $PROFILE_ARGS --dryrun
+  eval aws s3 sync . $S3_SYNC_ARGS $PROFILE_ARGS --dryrun --delete
 else
   if [ $1 == "--doit" ]; then
-    eval aws s3 sync . $S3_SYNC_ARGS $PROFILE_ARGS
+    eval aws s3 sync . $S3_SYNC_ARGS $PROFILE_ARGS --delete
   else
     echo "Unknown arguments. Either use no args to dryrun or use --doit to upload"
   fi
