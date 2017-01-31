a12207fbd94c787a82da971cf2ab027bef17acf5
Ville Seppanen <ville.seppanen@gofore.com>
2017-01-31 12:29:35 +0200

    Update api url in frontend. Show currently used backend.

--- frontend/main.js
+++ frontend/main.js
@@ -8,11 +8,12 @@ function xhrGet(url, callback) {
   xhr.send(null);
 };
 
-var url = 'https://07pfv59nv2.execute-api.eu-west-1.amazonaws.com/dev/items';
+var url = 'https://s8efi2qhsb.execute-api.eu-west-1.amazonaws.com/dev/items';
+document.getElementById('apiurl').innerHTML = 'Currently used backend API is <a href="' + url + '">' + url + '</a>';
 
 xhrGet(url, function(response) {
   var data = JSON.parse(response);
-  var itemsDiv = document.getElementById('items')
+  var itemsDiv = document.getElementById('items');
   for(var i=0; i < data.length; i++) {
     var item = '<div class="item"><a href="' + data[i]['url'] + '">' + data[i]['title'] + '</a></div>';
     itemsDiv.innerHTML = itemsDiv.innerHTML + item;
